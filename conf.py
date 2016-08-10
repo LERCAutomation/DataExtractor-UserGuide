a478640e380fd9e0a6df19d244e70544ba2e9b00
Andy Foy <andy@andyfoyconsulting.co.uk>
2016-08-10 21:47:14 +0100

    Update to copyright to include DERC
    Update to copyright to include DERC.

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Data Extractor User Guide'
-copyright = u'2016 GiGL, TVERC, SxBRC'
+copyright = u'2016 GiGL, TVERC, SxBRC, DERC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
